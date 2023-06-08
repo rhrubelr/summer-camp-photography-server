c48248bed56028a9cf96735cb98d583e6c169f56
rhrubelr <rhrubel246@gmail.com>
2023-06-09 02:50:03 +0600

    added instructor collection

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
+    
     deprecationErrors: true,
   }
 });
@@ -25,6 +26,9 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
