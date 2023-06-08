78de5c65db3d2a1802704ec4cfb18dc86b90a0f0
rhrubelr <rhrubel246@gmail.com>
2023-06-09 02:52:21 +0600

    added instructor app.get

--- index.js
+++ index.js
@@ -27,7 +27,14 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const instructorCollection = client.db("photography").collection('instructor');
 
+
+
+    app.get('/instructor', async(req, res)=> {
+        const result = await instructorCollection.find().toArray();
+        res.send(result)
+     })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
