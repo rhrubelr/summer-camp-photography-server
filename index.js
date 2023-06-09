7ff2098acfe7feb8ad70e0ff25122c1078d6067d
rhrubelr <rhrubel246@gmail.com>
2023-06-10 01:10:01 +0600

    added vercel.json file

--- index.js
+++ index.js
@@ -25,10 +25,12 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const instructorCollection = client.db("photography").collection('instructor');
+
     const classesCollection = client.db("photography").collection('classes');
+  
 
 
 
