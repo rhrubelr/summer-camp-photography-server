e16b0080eaa83049c4c5b15bf2506c559698855d
rhrubelr <rhrubel246@gmail.com>
2023-06-10 14:46:47 +0600

    added delete api

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ app.use(express.json())
 
 
 
-const jwtVerify = (req, res, next)=>{
+const jwtVerify = (req, res, next) => {
   const authorization = req.headers.authorization
-  if(!authorization){
-    return res.status(401).send({error: true, message: 'unathorization access'})
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unathorization access' })
   }
   // bearer token
   const token = authorization.split(' ')[1];
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SCRECT, (err, decoded)=>{
-    if(err){
-      return res.status(401).send({error: true, message: 'unathorization access'})
+  jwt.verify(token, process.env.ACCESS_TOKEN_SCRECT, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unathorization access' })
     }
     req.decoded = decoded;
     next()
@@ -37,7 +37,7 @@ const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
-    
+
     deprecationErrors: true,
   }
 });
@@ -50,48 +50,59 @@ async function run() {
     const instructorCollection = client.db("photography").collection('instructor');
 
     const classesCollection = client.db("photography").collection('classes');
-  
+
     const enrollCollection = client.db("photography").collection("allEnroll")
 
 
-    app.post('/jwt', (req, res)=>{
+    app.post('/jwt', (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SCRECT, {expiresIn: '1hr' })
-    res.send({token})
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SCRECT, { expiresIn: '1hr' })
+      res.send({ token })
     })
 
-    app.get('/instructor', async(req, res)=> {
-        const result = await instructorCollection.find().toArray();
-        res.send(result)
-     })
+    app.get('/instructor', async (req, res) => {
+      const result = await instructorCollection.find().toArray();
+      res.send(result)
+    })
 
-    app.get('/classes', async(req, res)=> {
-        const result = await classesCollection.find().toArray();
-        res.send(result)
-     })
+    app.get('/classes', async (req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result)
+    })
 
 
-     app.post('/all-enroll', async(req, res)=>{
+    // enroll data post 
+    app.post('/all-enroll', async (req, res) => {
       const enroll = req.body;
       const result = await enrollCollection.insertOne(enroll);
       res.send(result)
     })
 
 
-    app.get('/enroll', jwtVerify, async(req, res)=>{
+    app.get('/enroll', jwtVerify, async (req, res) => {
       const email = req.query.email;
       // console.log(email)
-      if(!email){
-       return res.send([]);
+      if (!email) {
+        return res.send([]);
       }
       const decodedEmail = req.decoded.email;
-      if(email !== decodedEmail){
-        return res.status(403).send({error: True, message: 'porviden access'})
+      if (email !== decodedEmail) {
+        return res.status(403).send({ error: True, message: 'porviden access' })
       }
-    
-      const query = {email: email};
+
+      const query = { email: email };
       // console.log(query)
       const result = await enrollCollection.find(query).toArray();
+      res.send(result)
+    })
+
+
+
+    // delete enroll 
+    app.delete('/enroll/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await enrollCollection.deleteOne(query)
       res.send(result)
     })
 
@@ -107,9 +118,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('photography is running')
+  res.send('photography is running')
 })
 
 app.listen(port, () => {
-    console.log(`Photography is running ${port}`)
+  console.log(`Photography is running ${port}`)
 })
